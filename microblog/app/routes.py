b024dc33aa23bda8c12d142cacea11bdbf3d1c74
nptravis <nptravis@yahoo.com>
2017-12-21 19:22:01 +0700

    Working on form for microbog

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,7 @@
 # this is a view function
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/') # decorators
 @app.route('/index') # basically assign this url path to this function
@@ -17,3 +18,9 @@ def index():
 		}
 	]
 	return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Sign In', form=form)
+	# passing the form object created in the line above (and shown on the right side) to the template with the name form (shown on the left)
