7a8717b3e8d252c5c2d13e6117ffeb99cfb3650a
nptravis <nptravis@yahoo.com>
2017-12-23 11:54:05 +0700

    added some validation and error handling to form fields

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,4 +1,3 @@
-
 #  creates the application object as an instance of class Flask imported from the flask package
 from flask import Flask 
 from config import Config
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -19,8 +19,12 @@ def index():
 	]
 	return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+# the methods argument tell it to accept get and post, overriding the default which is just post
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign In', form=form)
 	# passing the form object created in the line above (and shown on the right side) to the template with the name form (shown on the left)
